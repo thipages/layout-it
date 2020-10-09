426530397af95f84ba8a088ff5c00ac9f296a77d
thipages <thipages@gmail.com>
2020-10-09 12:48:20 +0200

    bugs fix

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 let _uid=Math.random();
-const uid=(p="")=>p+(_uid++);
-const getAttribute=(n, k)=>n.getAttribute(k);
-const setAttribute=(n, k, v)=>n.setAttribute(k,v);
+const uid=(p="")=>(p+(_uid++)).replace('.','');
+const ga=(n, k)=>n.getAttribute(k);
 const norm=(s)=>s.trim().replace( /\s\s+/g, ' ' );
 const getTemplateAreas=(rowNum, colNum, areas, uid)=> {
-    const tuple =(arr,n)=> Array.from({ length: arr.length / n }, (_, i) => arr.slice(i * n, i * n + n));    let p=tuple(areas.split(' '), colNum);
+    const tuple =(arr,n)=> Array.from({ length: arr.length / n }, (_, i) => arr.slice(i * n, i * n + n));
+    let p=tuple(areas.split(' '), colNum);
     let r=[];
-    for (let i=0;i<p.length;i++) r.push(`'${p[i].map(v=>uid+'_'+v).join(" ")}'`);
+    for (let i=0;i<p.length;i++) r.push(`'${p[i].map(v=>uid+"_"+v).join(" ")}'`);
     return [r.length===rowNum*colNum,r.join(' ')];
 };
-const registerChildren=(t, parent, uid)=> {
-    let st=[...new Set(t.split(" "))].sort();
-    let children=parent.children;
-    let i=0;
-    for (let c of children) {
-        c.style.gridArea=uid+st[i];
-        i++;
+const registerChildren=(children,oKeys, uid)=> {
+    for (let i=0,len=children.length;i<len;i++) {
+        children[i].style['grid-area']=uid+"_"+oKeys[i];
     }
 };
 customElements.define(
@@ -23,21 +19,26 @@ customElements.define(
     class extends HTMLElement {
         static get observedAttributes() { return ['areas','rows','columns']; }
         attributeChangedCallback(name, oldValue, newValue) {
-            let nv=norm(newValue);
-            setAttribute(this,'name',nv);
-            if (name==='rows' || name==='columns') setAttribute(this,'grid-template-'+name,nv);
+            
         }
         connectedCallback() {
             let id=uid("g_");
-            let t=getTemplateAreas(
-                getAttribute(this,'rows').split(' ').length,
-                getAttribute(this,'columns').split(' ').length,
-                getAttribute(this,'areas'),id
-            );
             this.style.display = 'grid';
+            let r=norm(ga(this,'rows'));
+            let c=norm(ga(this,'columns'));
+            let a=norm(ga(this,'areas'));
+            let t=getTemplateAreas(
+                r.split(' ').length,
+                c.split(' ').length,
+                a,
+                id
+            );
             if (t[0]) {
-                setAttribute(this,'grid-template-areas',t[1]);
-                registerChildren(t[1], this,id);
+                this.style['grid-template-rows']=r;
+                this.style['grid-template-columns']=c;
+                this.style['grid-template-areas']=t[1];
+                let st=[...new Set(t[1].split(' '))].sort();
+                registerChildren(this.children,st,id);
             }
         }
     }
